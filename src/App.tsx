6369bf75a54f6b538a2eb3c23257a8bdb977e1ed
Raman Mastyka <raman.mastyka@gmail.com>
2023-10-31 21:00:06 +0100

    style: fix format

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ class App extends React.Component<object> {
   render() {
     return (
       <>
-        <h1 className='text-3xl my-4 bold font-bold text-center'>Search App</h1>
+        <h1 className="text-3xl my-4 bold font-bold text-center">Search App</h1>
         <SearchPage />
       </>
     );
