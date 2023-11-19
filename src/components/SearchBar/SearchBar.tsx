645d27f0f90a5666a14fbea7a6ef0c11b284f5ef
Raman Mastyka <raman.mastyka@gmail.com>
2023-11-19 22:02:55 +0100

    refactor: refactor search component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,28 +1,17 @@
-import React, { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '@/app/hooks';
-import { setSearchText } from '@/features/laureates/laureatesSlice';
+import React from 'react';
+
 interface SearchBarProps {
   onSearch: (query: string) => void;
+  searchText: string;
 }
 
-const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
-  const dispatch = useAppDispatch();
-  const searchText = useAppSelector((state) => state.laureate.searchText);
-
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch, searchText: defaultSearchText }) => {
+  const [searchText, setSearchText] = React.useState(defaultSearchText);
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     onSearch(searchText);
     localStorage.setItem('searchQuery', searchText);
   };
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(setSearchText(e.target.value.trim()));
-  };
-
-  useEffect(() => {
-    const savedQuery = localStorage.getItem('searchQuery') || '';
-    dispatch(setSearchText(savedQuery));
-  }, [dispatch]);
 
   return (
     <div className=" justify-start mb-4">
@@ -34,7 +23,7 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
           className="h-full w-full rounded border bg-transparent px-3 py-2.5 font-sans text-sm font-normal transition-all focus:border-pink-500 focus:outline-0"
           type="text"
           value={searchText}
-          onChange={handleChange}
+          onChange={(e) => setSearchText(e.target.value)}
           placeholder=" "
         />
         <button
--- src/pages/Laureates/Laureates.tsx
+++ src/pages/Laureates/Laureates.tsx
@@ -4,9 +4,13 @@ import { useSearchLaureatesQuery } from '@/api/laureates';
 import Pagination from '@/components/Pagination/Pagination';
 import LaureatesList from './LaureatesList';
 import Loading from '@/components/Loading';
-import {DEFAULT_LIMIT, DEFAULT_PAGE} from "@/config";
+import { DEFAULT_LIMIT, DEFAULT_PAGE } from '@/config';
+import { useAppDispatch, useAppSelector } from '@/app/hooks';
+import { setSearchText } from '@/features/laureates/laureatesSlice';
+import { useEffect } from 'react';
 
 export default function Laureates() {
+  const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const name = searchParams.get('search') ?? '';
@@ -16,6 +20,12 @@ export default function Laureates() {
   const req = { name, page, limit };
   const res = useSearchLaureatesQuery(req);
   const { data, error, isFetching, isSuccess, isError } = res;
+  const searchText = useAppSelector((state) => state.laureate.searchText);
+
+  useEffect(() => {
+    const savedQuery = localStorage.getItem('searchQuery') ?? '';
+    dispatch(setSearchText(savedQuery));
+  }, [dispatch]);
 
   const onSearch = (query: string) => {
     searchParams.set('search', query);
@@ -38,20 +48,19 @@ export default function Laureates() {
     });
   };
 
+  const createPageUrl = (searchParams: URLSearchParams, page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', page.toString());
+    return params.toString();
+  };
   let content;
 
   if (isFetching) {
     content = <Loading />;
   } else if (isSuccess) {
     const { total, laureates } = data;
-    const left = new URLSearchParams({
-      ...searchParams,
-      page: (page - 1).toString(),
-    }).toString();
-    const right = new URLSearchParams({
-      ...searchParams,
-      page: (page + 1).toString(),
-    }).toString();
+    const left = createPageUrl(searchParams, page - 1);
+    const right = createPageUrl(searchParams, page + 1);
     content = (
       <>
         <Pagination
@@ -75,7 +84,7 @@ export default function Laureates() {
   return (
     <div className="flex flex-row justify-start p-4">
       <div className="flex flex-col flex-grow mr-4">
-        <SearchBar onSearch={onSearch} />
+        <SearchBar onSearch={onSearch} searchText={searchText} />
         {content}
       </div>
       <div className="h-screen flex flex-col justify-center">
