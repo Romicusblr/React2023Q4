c60ab20861790fd6251fc6353017b2a8a3363cdf
Raman Mastyka <raman.mastyka@gmail.com>
2023-12-03 18:04:58 +0100

    feat: update forms

---
+++ src/components/Input/InputText.tsx
@@ -0,0 +1,18 @@
+type InputTextProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  label?: string;
+  type?: string;
+};
+
+const InputText: React.FC<InputTextProps> = ({ name, type = "text", label=`Your ${name}`, ...rest }) => {
+  return (
+    <div className="mb-5">
+      <label htmlFor={name} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        {label}
+      </label>
+      <input type={type} id={name} className="input-text" {...rest} />
+    </div>
+  );
+};
+
+export default InputText;
